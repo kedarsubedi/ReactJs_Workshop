c8193f7320f0caa4baf821b49fc1ea89ace9f0da
kedarsubedi <kedar.sed@gmail.com>
2023-05-06 11:27:59 +0545

    Minor change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import ProductPage from './components/ProductPage';
 
 const App = () => {
   return (
+    <>
     <ProductPage title='Logitech K380' category='Keyboard' picture={productpic} price='1200'/>
+    <ProductPage title='Logitech K380' category='Keyboard' picture={productpic} price='2400'/>
+    <ProductPage title='Logitech K380' category='Keyboard' picture={productpic} price='2500'/>
+    <ProductPage title='Logitech K380' category='Keyboard' picture={productpic} price='2700'/>
+    </>
   );
 };
 
